b16e72411d55b37e930aef1790f60dffe2e89eac
Philipp Gildein <philipp@gildein.com>
2014-10-26 20:44:15 +0100

    Add keychain support

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,2 +1,6 @@
 source $ZSH/plugins/syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH/plugins/history-substring-search/zsh-history-substring-search.zsh
+
+# Keychain support
+[[ -f $(which keychain 2> /dev/null) ]] && \
+    keychain --nogui -Q -q id_rsa github_rsa
