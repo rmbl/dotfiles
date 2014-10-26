9018986c2f9868a03a65d69c7b351ffc64d9c57d
Philipp Gildein <philipp@gildein.com>
2014-10-26 19:40:41 +0100

    Fix plugins for zsh

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -12,16 +12,18 @@ bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
 
-# bind UP and DOWN arrow keys
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
+if [ -f /usr/local/opt/zsh-history-substring-search/zsh-history-substring-search.zsh ]; then
+    # bind UP and DOWN arrow keys
+    bindkey "$terminfo[kcuu1]" history-substring-search-up
+    bindkey "$terminfo[kcud1]" history-substring-search-down
 
-# bind P and N for EMACS mode
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
+    # bind P and N for EMACS mode
+    bindkey -M emacs '^P' history-substring-search-up
+    bindkey -M emacs '^N' history-substring-search-down
 
-# bind k and j for VI mode
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+    # bind k and j for VI mode
+    bindkey -M vicmd 'k' history-substring-search-up
+    bindkey -M vicmd 'j' history-substring-search-down
+fi
 
 bindkey -v   # Default to standard vi bindings, regardless of editor string
