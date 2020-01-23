ad214aae0746cfb14241cfeb72e0d126c2fe5a05
Philipp Gildein <philipp@gildein.com>
2020-01-23 22:15:19 +0100

    Updates to the install-arch.sh script

--- install-arch.sh
+++ install-arch.sh
@@ -1,17 +1,14 @@
-sudo pacman -S --noconfirm base-devel zsh git neovim python-neovim xclip xsel wget cmake diff-so-fancy keychain the_silver_searcher ttf-liberation xdg-utils awesome unclutter compton xautolock rofi network-manager-applet rxvt-unicode rxvt-unicode-terminfo rlwrap vicious firefox alsa-utils
+sudo pacman -S --noconfirm base-devel zsh git neovim python-neovim xclip xsel wget cmake diff-so-fancy keychain the_silver_searcher ttf-liberation xdg-utils awesome unclutter xautolock rofi network-manager-applet rxvt-unicode rxvt-unicode-terminfo rlwrap vicious firefox alsa-utils
 
 git submodule update --init --recursive
 
-cd vim/bundle/youcompleteme
-./install.py
-cd ../../..
-
-wget https://github.com/Jguer/yay/releases/download/v8.1173.0/yay_8.1173.0_x86_64.tar.gz -O yay.tar.gz
+wget https://github.com/Jguer/yay/releases/download/v9.4.0/yay_9.4.0_x86_64.tar.gz
 tar xfvz yay.tar.gz
-cd yay_8.1173.0_x86_64
+cd yay_9.4.0_x86_64
 ./yay -S yay
 cd ..
 rm -r yay*
 
 yay -S --noconfirm google-chrome betterlockscreen-git ruby-neovim brightnessctl
-
+npm install -g neovim
+gem install --user neovim
